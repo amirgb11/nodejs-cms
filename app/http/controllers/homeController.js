4f6b0a804269f5a9ccf0032eb8807e6d72ecea68
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2019-01-31 17:41:11 +0330

    13. Add Master Page

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -3,7 +3,7 @@ const controller = require('app/http/controllers/controller');
 class homeController extends controller {
     index(req , res){
         // res.json(req.user);
-         res.render('home');
+         res.render('home/index');
     }
 }
 
